c8351670c5a30753cc65ee2e126a0c19b5f04322
jj-chung <jenniechung6@gmail.com>
2022-10-23 14:20:14 -0700

    Added data processing file

---
+++ data_processing.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np 
+
+def get_all_data():
+  # my_data = genfromtxt('CLEAR_Corpus_6.01.csv', delimiter=',', dtype=None)
+  # read excel in data frame 
+  df = pd.read_excel('CLEAR_Corpus_6.01.xlsx') 
+  
+  # convert a data frame to a Numpy 2D array 
+  my_data = np.asarray(df) 
+  return my_data
+
+if __name__ == "__main__":
+  print(get_all_data())
