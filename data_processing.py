f2d5d7585d4774f7ffa97513fe133be2c32398a6
jj-chung <jenniechung6@gmail.com>
2022-10-27 21:43:12 -0700

    Added data excerpt

--- data_processing.py
+++ data_processing.py
@@ -84,7 +84,38 @@ def word_vectorizer2():
   return X_train_tfidf
 
 
+"""
+Create 2 new features, average word length and average sentence length, using
+the text excerpt data. 
+"""
+def create_new_features():
+  # First, get all the data
+  # Save the new data
+  all_data = get_all_data()
+  data_excerpts = all_data[:, 14]
+
+  avg_word_length = []
+  avg_sentence_length = []
+
+  for excerpt in data_excerpts:
+    # Compute average word length for the excerpt
+    words = excerpt.split()
+    print(words)
+
+    total_avg = sum( map(len, words) ) / len(words)
+    avg_word_length.append(total_avg)
+
+    # Compute average sentence length for the excerpt
+    sentences = excerpt.split(".")
+    total_avg = sum( map(len, sentences) ) / len(sentences)
+    avg_sentence_length.append(total_avg)
+
+  
+
+  
+
 if __name__ == "__main__":
-  print(test_data())
-  print(train_data())
-  print(text_train_data())
+  # print(test_data())
+  # print(train_data())
+  # print(text_train_data())
+  create_new_features()
