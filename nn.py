bbf0527d5d47a6e5ef9f566fcc7d6dcb48110863
echoi3-hw <echoi1@stanford.edu>
2022-11-27 16:24:29 -0800

    neural nets param opt with gridsearch

--- nn.py
+++ nn.py
@@ -5,16 +5,26 @@ import data_preprocessing
 from raw_data import *
 from data_preprocessing import *
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import GridSearchCV
 
 def train_nn(train_v, train_s, test_v, test_s):
-    mlp = MLPClassifier(hidden_layer_sizes=(8, 8, 8), activation='relu', solver='adam', max_iter=500)
-    mlp.fit(train_v, train_s)
+    #model = MLPRegressor(hidden_layer_sizes=(8, 8, 8), activation='relu', solver='adam', max_iter=500)
+    #mlp.fit(train_v, train_s)
+    print(MLPRegressor().get_params().keys())
+    parameters = {'batch_size': [20, 32, 64, 128],
+                    'max_iter': [100, 150],
+                  'solver': ['adam', 'rmsprop'],
+                  'hidden_layer_sizes': [(8, 8, 8, 8), (10, 10, 10)]}
+    grid_search = GridSearchCV(estimator=MLPRegressor(),
+                               param_grid=parameters,
+                               scoring='neg_mean_absolute_error',
+                               cv=5)
+    model = grid_search.fit(train_v, train_s)
 
-    predict_test = mlp.predict(test_v)
-    accuracy = np.mean(predict_test == test_s)*100
-    return accuracy
+    predict_test = model.predict(test_v)
+    error = mean_squared_error(test_s, predict_test)
+    return error
 
 '''def create_plot(X, y, y_pred):
     plt.scatter(X, y, color="black")
@@ -34,4 +44,5 @@ if __name__ == "__main__":
     test_vector = data_preprocessing.word_vectorizer_train(type="test")
     test_score = bt_easiness_test_data()
     accuracy = train_nn(train_vector, train_score, test_vector, test_score)
+    print('nn accuracy:', accuracy)
 
